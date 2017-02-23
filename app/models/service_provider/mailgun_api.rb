756b821ba2ad9fd6b78fdd6721e20afff8fc2c21
abrbhat <bhatnagarabhiroop@gmail.com>
2017-02-23 22:20:13 +0530

    Implemented attachments for mailgun

--- app/models/service_provider/mailgun_api.rb
+++ app/models/service_provider/mailgun_api.rb
@@ -12,7 +12,7 @@ module ServiceProvider
         # Allows multiple authorized email separated by |
         # Example: "test1@example.com|test2@example.com"
         ENV['MAILGUN_AUTHORIZED_EMAIL'].split("|").each do |authorized_email|
-          mb_obj.add_recipient(:to, email)
+          mb_obj.add_recipient(:to, authorized_email)
         end
 
         mb_obj.subject("#{email.subject} (mailgun)");
@@ -20,13 +20,13 @@ module ServiceProvider
         mb_obj.body_text(email.body);
 
         email.attachments.each do |attachment|
-          #TODO
+          mb_obj.add_attachment attachment.path, attachment.original_filename
         end
 
         # Send your message through the client
         result = mg_client.send_message ENV['MAILGUN_SENDER_EMAIL'].split('@')[1],
                                         mb_obj
-
+        
         if result.code == 200
           email.update_delivery_statuses({
             ENV['MAILGUN_AUTHORIZED_EMAIL'] => "sent"
