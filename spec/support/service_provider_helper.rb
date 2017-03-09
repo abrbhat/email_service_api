94a3f9b50831e7675ea8a39fbbdcd8ed96488acb
abrbhat <bhatnagarabhiroop@gmail.com>
2017-03-09 17:47:29 +0530

    Added comment for service provider helper

--- spec/support/service_provider_helper.rb
+++ spec/support/service_provider_helper.rb
@@ -1,5 +1,6 @@
 RSpec.configure do |config|
   config.before(:each) do
+    # Use only MandrillAPI for test
     allow(ServiceProvider::Base).to receive(:list)
       .and_return(['MandrillAPI'])
   end
